eebd59ce57c3832957af8d7df9a385d1b69e186b
Saran Sundararajan <saransszg@gmail.com>
2019-08-03 19:35:19 -0500

    Websockets complete

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 import os
 import platform
+import signal
 import sys
+from subprocess import Popen, PIPE
 
 from flask import Flask, render_template
 from flask_cors import CORS
 from flask_sockets import Sockets
+from gevent import pywsgi
+from geventwebsocket.handler import WebSocketHandler
 
 from flask_blueprints.example_bp import example_bp
 from flask_blueprints.example_bp import example_ws
@@ -58,8 +62,22 @@ def home():
     return render_template("index.html")
 
 
+def kill_port(port):
+    process = Popen(["lsof", "-i", ":{0}".format(port)], stdout=PIPE, stderr=PIPE)
+    stdout, stderr = process.communicate()
+    for process in str(stdout.decode("utf-8")).split("\n")[1:]:
+        data = [x for x in process.split(" ") if x != '']
+        if len(data) <= 1:
+            continue
+
+        os.kill(int(data[1]), signal.SIGKILL)
+
+
 def run_app(url, port):
-    app.run(host=url, port=port, threaded=True)
+    kill_port(port)
+    server = pywsgi.WSGIServer((url, port), app, handler_class=WebSocketHandler)
+    server.serve_forever()
+    # app.run(host=url, port=port, threaded=True)
 
 
 if __name__ == '__main__':
